1e1ec5e0230f30c37469be8faa143a819d77bfe0
JShistory <behappy0916@naver.com>
2024-10-22 15:24:46 +0900

    feat: kafka Producer, Consumer 기능 추가

---
+++ src/main/java/com/study/kafkatest/KafkaController.java
@@ -0,0 +1,22 @@
+package com.study.kafkatest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KafkaController {
+    private final KafkaProducer producer;
+
+    @PostMapping("/kafka1")
+    public void sendMessage1(@RequestBody String message) {
+        producer.sendMessage1(message);
+    }
+
+    @PostMapping("/kafka2")
+    public void sendMessage2(@RequestBody String message) {
+        producer.sendMessage2(message);
+    }
+}
